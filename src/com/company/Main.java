fa5848f37436f2377c4a6eb8c34b032c583b358d
JaredDet <71043931+JaredDet@users.noreply.github.com>
2021-04-11 02:44:30 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,33 +6,56 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        validarEntrada();
+        
+        verificarPalindromo(entradaString());
     }
 
-    //Metodo que crea una entrada
+   //Metodo que crea una entrada de String
 
-    public static int Entrada(){
+    public static String entradaString(){
 
         Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese entrada");
 
-        int entrada = teclado.nextInt();
+        String entrada = teclado.nextLine();
 
             return entrada;
     }
+      //Metodo que elimina las tildes, comas, espacios, etc
 
-    //Metodo que valida una entrada
+    public static String eliminarSpeChar(String palabra){
 
-    public static int validarEntrada(){
+        String spechar = "áéíóú";
+        String chara = "aeiou";
+        String signos = "¡!¿?.,;:'";
 
-        int entrada = 0;
-        try{
-            entrada = Entrada();
+        for(int i = 0; i < spechar.length(); i++){
+
+            palabra = palabra.replace(spechar.charAt(i), chara.charAt(i));
         }
-        catch(Exception exception){
-            System.out.println("¡Error!");
-            validarEntrada();
+
+        for(int i = 0; i < signos.length(); i++){
+
+            palabra = palabra.replace(signos.charAt(i), ' ');
         }
-        return entrada;
+
+        palabra = palabra.replace(" ","");
+
+        return palabra;
+    }
+
+    //Metodo que verifica que palabra es un palíndromo
+
+    public static void verificarPalindromo(String palabra){
+
+        palabra = eliminarSpeChar(palabra);
+
+        StringBuilder palindromo = new StringBuilder(palabra.toLowerCase());
+
+        if (palindromo.toString().equals(palindromo.reverse().toString())){
+            System.out.println("Es un palíndromo");
+            return;
+        }
+        System.out.println("No es palíndromo");
     }
 }
